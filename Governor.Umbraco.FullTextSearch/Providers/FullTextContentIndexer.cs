cc6df0898014a508819b8a8b2926c45195b23ee4
Rigardt Pheiffer <rigardt@governor.co.uk>
2013-05-21 15:15:12 +0100

    Replacing Umbraco static dlls with nuget package. Including dlls.

--- Governor.Umbraco.FullTextSearch/Providers/FullTextContentIndexer.cs
+++ Governor.Umbraco.FullTextSearch/Providers/FullTextContentIndexer.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Governor.Umbraco.FullTextSearch.Interfaces;
-using UmbracoExamine;
 using Examine;
 using System.IO;
+using UmbracoExamine;
 
 namespace Governor.Umbraco.FullTextSearch.Providers
 {

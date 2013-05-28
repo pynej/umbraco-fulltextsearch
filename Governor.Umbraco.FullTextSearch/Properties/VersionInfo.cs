4c6ca16d89d8f31d297d3460d1c50569dc68419e
Rigardt Pheiffer <rigardt@governor.co.uk>
2013-05-28 13:47:04 +0100

    Adding package actions to add/remove Examine configuration settings.

--- Governor.Umbraco.FullTextSearch/Properties/VersionInfo.cs
+++ Governor.Umbraco.FullTextSearch/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("4.7.0")]
-[assembly: AssemblyFileVersion("4.7.4.34522")]
+[assembly: AssemblyFileVersion("4.7.8.37466")]
 [assembly: AssemblyInformationalVersion("4.7.0")]
 
 

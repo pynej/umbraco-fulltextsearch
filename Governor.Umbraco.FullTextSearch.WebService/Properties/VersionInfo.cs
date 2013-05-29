448bab164dca0aa6aa1942719957f29e1fc0de8c
Rigardt Pheiffer <rigardt@governor.co.uk>
2013-05-29 13:16:27 +0100

    Cleaning bin folder.

--- Governor.Umbraco.FullTextSearch.WebService/Properties/VersionInfo.cs
+++ Governor.Umbraco.FullTextSearch.WebService/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("4.7.0")]
-[assembly: AssemblyFileVersion("4.7.3.44060")]
+[assembly: AssemblyFileVersion("4.7.9.36216")]
 [assembly: AssemblyInformationalVersion("4.7.0")]
 
 

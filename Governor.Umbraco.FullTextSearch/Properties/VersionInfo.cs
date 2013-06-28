7e5dc0044e8eeb8eba2c71fcd397477ef24353dd
Rigardt Pheiffer <rigardt@governor.co.uk>
2013-06-28 14:58:12 +0100

    Setting version number of latest build.

--- Governor.Umbraco.FullTextSearch/Properties/VersionInfo.cs
+++ Governor.Umbraco.FullTextSearch/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("4.8.0")]
-[assembly: AssemblyFileVersion("4.8.39.31585")]
+[assembly: AssemblyFileVersion("4.8.39.40855")]
 [assembly: AssemblyInformationalVersion("4.8.0")]
 
 

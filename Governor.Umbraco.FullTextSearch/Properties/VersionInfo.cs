27812f65d9661581f2c0c831610c5be0d25eed39
Rigardt Pheiffer <rigardt@governor.co.uk>
2013-07-05 10:21:23 +0100

    Removing bin folders and adding to ignore list.

--- Governor.Umbraco.FullTextSearch/Properties/VersionInfo.cs
+++ Governor.Umbraco.FullTextSearch/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("4.7.0")]
-[assembly: AssemblyFileVersion("4.7.39.41285")]
+[assembly: AssemblyFileVersion("4.7.46.28241")]
 [assembly: AssemblyInformationalVersion("4.7.0")]
 
 
